61689ef3a3b918e3a2120c2c7d0f67b725aa69ea
Jordan Jones <jrjh27cs@gmail.com>
2018-04-11 21:11:29 -0500

    Also adding quil dependency

--- project.clj
+++ project.clj
@@ -4,7 +4,8 @@
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.9.0"]
-                 [seesaw "1.4.5"]]
+                 [seesaw "1.4.5"]
+                 [quil "2.7.0"]]
   :main ^:skip-aot audio-visualizer.core
   :target-path "target/%s"
   :profiles {:uberjar {:aot :all}})
