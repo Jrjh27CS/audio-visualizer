ceeeef45f32f0c326ccb66d3d8fb1fa72b6321eb
Jordan Jones <jrjh27cs@gmail.com>
2018-04-11 20:52:37 -0500

    Changing project description and 1.9 dep

--- project.clj
+++ project.clj
@@ -1,9 +1,9 @@
 (defproject audio-visualizer "0.1.0-SNAPSHOT"
-  :description "FIXME: write description"
+  :description "Audio Visualizer made in clojure using seesaw library"
   :url "http://example.com/FIXME"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
-  :dependencies [[org.clojure/clojure "1.8.0"]]
+  :dependencies [[org.clojure/clojure "1.9.0"]]
   :main ^:skip-aot audio-visualizer.core
   :target-path "target/%s"
   :profiles {:uberjar {:aot :all}})
