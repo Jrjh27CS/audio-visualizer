41c84d826f411bf74bc68adf2763495a2cc9a93b
Jordan Jones <jrjh27cs@gmail.com>
2018-04-11 21:18:24 -0500

    Copying example from quil

--- src/audio_visualizer/core.clj
+++ src/audio_visualizer/core.clj
@@ -1,5 +1,29 @@
 (ns audio-visualizer.core
+  (:require [quil.core :as q])
   (:gen-class))
+
+(defn setup
+  []
+  (q/frame-rate 1)
+  (q/background 200))
+
+(defn draw
+  []
+  (q/stroke (q/random 255))
+  (q/stroke-weight (q/random 10))
+  (q/fill (q/random 255))
+
+  (let [diam (q/random 100)
+        x (q/random (q/width))
+        y (q/random (q/height))]
+    (q/ellipse x y diam diam)))
+
+(q/defsketch example
+  :title "First try Friday"
+  :settings #(q/smooth 2)
+  :setup setup
+  :draw draw
+  :size [323 200])
 
 (defn -main
   "I don't do a whole lot ... yet."
